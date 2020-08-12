43833c40741ab65fe39c44dd41747891920138a4
willmharris <willmharris0@gmail.com>
2020-08-12 12:08:28 -0400

    added videos without player

--- src/Components/Videos.js
+++ src/Components/Videos.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import {Button} from 'semantic-ui-react'
+import ReactPlayer from 'react-player/youtube'
 
 function Videos() {
     return(
         <div>
             <Button>Test button</Button>
+            <span>Humanz Mediate</span>
+            <ReactPlayer
+                url="https://youtu.be/b-24GeQ3qBk"
+            />
+            <span>Magic Lyrics</span>
+            <ReactPlayer
+                url="https://youtu.be/MyBmAAnP6RA"
+            />
+            <span>Tweet Mapper</span>
+            <ReactPlayer
+                url="https://youtu.be/iH8Nqe0EniM"
+            />
         </div>
     )
 }
