035b5785c2e1cb947bb71a8f9a680353ee59e706
willmharris <willmharris0@gmail.com>
2020-08-12 11:49:54 -0400

    added semantic ui

---
+++ src/Components/Videos.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Button} from 'semantic-ui-react'
+
+function Videos() {
+    return(
+        <div>
+            <Button>Test button</Button>
+        </div>
+    )
+}
+
+export default Videos
